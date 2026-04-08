ab034a5001bb2eb618f6dd672c96701fa2086715
Restr32 <s33489@pjwstk.edu.pl>
2026-04-08 22:59:00 +0200

    Dodanie interfejsu i klasy obsługującej klase LegacyBillingGateway

---
+++ LegacyRenewalApp/IBillingService.cs
@@ -0,0 +1,6 @@
+﻿namespace LegacyRenewalApp;
+
+public interface IBillingService {
+    void saveInvoce(RenewalInvoice voice);
+    void sendEmail(string mail, string name, string plan);
+}
---
+++ LegacyRenewalApp/LegacyBilling.cs
@@ -0,0 +1,13 @@
+﻿namespace LegacyRenewalApp;
+
+public class LegacyBilling : IBillingService {
+    public void saveInvoce(RenewalInvoice voice) {
+        LegacyBillingGateway.SaveInvoice(voice);
+    }
+
+    public void sendEmail(string mail, string name, string plan) {
+        var subject = "Generated information!";
+        var body = $"Morning {name}, We are happy to announce that ur plan: {plan} is ready!";
+        LegacyBillingGateway.SendEmail(mail, subject, body);
+    }
+}
